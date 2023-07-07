d152dc64b0914f38633246785e1c784456a6e35b
“OeilDigital” <oeildigital@yahoo.fr>
2023-07-07 13:00:08 +0200

    Responsive design and logo

--- public/script.js
+++ public/script.js
@@ -5,3 +5,16 @@ setTimeout(() => {
 }, 1000);
 
 h1.classList.remove("h1Effect");
+
+function responsiveTable() {
+    document.querySelectorAll('.table-responsive').forEach((table) => {
+        let labels = Array.from(table.querySelectorAll('th')).map((th) => { return th.innerText });
+
+        table.querySelectorAll('td').forEach((td, i) => {
+            td.setAttribute('data-label', labels[i % labels.lenght])
+            console.log(table);
+        })
+    })
+}
+
+responsiveTable()
